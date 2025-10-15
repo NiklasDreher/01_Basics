f8f8f73ebac2f5b0a0f95c25c6672901e2745b43
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2025-10-15 17:50:31 +0200

    Finished 30_AufgabeVariablen

---
+++ 30_AufgabeVariablen/index.js
@@ -0,0 +1,33 @@
+let banana = "Banane"
+let apple = "Apple"
+
+let bananaPricePerKilo = 2.14
+let applePricePerKilo = 3.43
+
+let bananaWeight = 0.22
+let appleWeight = 0.34
+
+
+function main() {
+
+function priceToLog(fruitWeight, fruitPricePerKilo){
+console.log("Preis von " + aussage + fruitWeight * fruitPricePerKilo * number)
+}
+
+let aussage = "8 Äpfel = "
+let number = 8
+priceToLog(appleWeight, applePricePerKilo)
+aussage = "17 Bananen = "
+number = 17
+priceToLog(bananaWeight, bananaPricePerKilo)
+aussage = "1 Tonne Äpfel = "
+number = 1000
+appleWeight = 1
+priceToLog(appleWeight, applePricePerKilo)
+aussage = "1 Tonne Bananen = "
+number = 1000
+bananaWeight = 1
+priceToLog(bananaWeight, bananaPricePerKilo)
+}
+
+main()
