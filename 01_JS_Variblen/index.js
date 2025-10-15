3c0cdd7a0d43a2a4c37511aef6092a729a2d6d0b
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2025-10-15 15:45:45 +0200

    Added first file

---
+++ 01_JS_Variblen/index.js
@@ -0,0 +1,12 @@
+let banana = "Banane"
+let apple = "Apple"
+
+let bananaPricePerKilo = 2.14
+let applePricePerKilo = 3.43
+
+let bananaWeight = 0.22
+let appleWeight = 0.34
+
+function apple(){
+    console.log("8 " )
+}
