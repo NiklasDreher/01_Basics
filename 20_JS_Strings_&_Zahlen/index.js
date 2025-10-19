67d8833466b994933fe7e3b550f831069e1ef26e
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2025-10-19 11:45:41 +0200

    Finished 20_JS_Strings_&_Zahlen

---
+++ 20_JS_Strings_&_Zahlen/index.js
@@ -0,0 +1,15 @@
+function main() {
+let a = 17.4
+let b = 3.9
+
+let result = a / b
+console.log("The Result of " + a + " divided by " + b + " is " + result)
+
+let vorname = "Niklas"
+let nachname = "Dreher"
+
+console.log("Ich heiße " + vorname + " " + nachname)
+
+}
+
+main()
