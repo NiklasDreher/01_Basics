01854e7a476e37f3536bc320a31c60d6d8fd31c2
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2025-11-02 18:48:30 +0100

    60_Loops\aufgabe7.js

---
+++ 60_Loops/aufgabe7.js
@@ -0,0 +1,15 @@
+function main(){
+
+    let sum = 0
+    for(let i = 1; i <= 100; i++){
+        if(i % 5 === 0){
+            sum += i
+        } else{
+            continue
+        }
+    } 
+
+    console.log(sum)
+}
+
+main()
