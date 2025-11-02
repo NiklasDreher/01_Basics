d6a1a193675385f2468979c8ef4321bbec340ff3
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2025-11-02 18:52:24 +0100

    60_Loops\aufgabe8.js

---
+++ 60_Loops/aufgabe8.js
@@ -0,0 +1,11 @@
+function main(){
+    for(let i = 1; i <= 50; i++){
+        if(i>= 20 && i <= 30){
+            continue
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+main()
