8935c483788c88b01ca8b3a6b9defc79f4c3048b
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2025-11-02 16:42:06 +0100

    60_Loops\aufgabe2.js

---
+++ 60_Loops/aufgabe2.js
@@ -0,0 +1,30 @@
+/*1bWI_Basics
+Copyright(C) 2025 Niklas Dreher
+
+This program is free software: you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see < http://www.gnu.org/licenses/gpl.txt>.
+*/
+
+function main(){
+
+    let x = 0
+
+    for(let i = 1; i <= 50; i++){
+        if(i % 2 !== 0){
+            x += i
+        }
+    }
+
+    console.log(x)
+}
+
+main()
